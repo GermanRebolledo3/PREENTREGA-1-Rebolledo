bc0f29baf6571c164945e5eaf228041f8bd2c95d
GermanRebolledo <germigallina@gmail.com>
2023-02-18 01:01:34 -0300

    preentrega-2

--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
-//function Calculadora(){
-    //let operador;
-    
-        //while(operador != "salir"){
-          //  operador = prompt("Digite si quiere + - / *");
-           // if(operador != "+" && operador != "-" && operador != "*" && operador != "/") {
-              //  alert("Debe colocar un signo")
-               // Calculadora()
-           // }
-           // let numero1 = parseInt(prompt("Digite el primer numero"));
-           // let numero2 = parseInt(prompt("Digite el segundo numero"));
-           // let suma = numero1 + numero2;
-           // //let resta = numero1 - numero2;
-           // let multiplicacion = numero1 * numero2;
-           // let division = numero1 / numero2;
-           // if (operador === "+"){
-           //     alert(`El resultado es ${suma}`);
-           // }
-           // else if (operador === "-"){
-            //    alert(`El resultado es ${resta}`);
-            //}
-            //else if (operador === "*"){
-            //    alert(`El resultado es ${multiplicacion}`);
-           // }
-            //else if (operador === "/"){
-            //    alert(`El resultado es ${division}`);
-           // }
-   //}
-    //}
-    //Calculadora()
+
 
    
     let lista_alumnos = [];
